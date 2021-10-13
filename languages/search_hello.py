2b79fe905e23e15d2b4ba61c0bd50d01a1bf15ef
Ethan Smith <ethan@esmithy.net>
2021-10-13 11:02:51 -0600

    gaminga

--- languages/search_hello.py
+++ languages/search_hello.py
@@ -1,44 +1,26 @@
 import re
-
-# search_file = input("What file do you want to search? ")
 search_file = 'warpeace.txt'
-#search_term = input("What are you looking for? ")
-
-# return when you find a match, count of matches, count of each section match
-        # Phone numbers in the forms:
-        # ###-###-####
-        # 1-###-###-####
-        # (###) ###-####
-        # Account numbers:
-        # AA-####-A#####
-        # SSN:
-        # ###-##-####
-        # Dates:
-        # ##/##/#### (in the form of MM/DD/YYYY)
-        # ##-##-#### (in the form of MM-DD-YYYY)
-        # ##/##/## (in the form of MM/DD/YY)
-        # ##-##-## (in the form of MM-DD-YY)
-
 # A data structure to surpass metal gear, should be easily prgrammatically iterable
 terms = {
    "phone number":[
-        r"\d{3}-\d{3}-\d{4}",
-        r"1-\d{3}-\d{3}-\d{4}",
-        r"\(\d{3}\)\S\d{3}-\d{4}",
+        r"\d{3}-\d{3}-\d{4}", # ###-###-####
+        r"1-\d{3}-\d{3}-\d{4}", # 1-###-###-####
+        r"\(\d{3}\)\s\d{3}-\d{4}", # (###) ###-####
     ],
     "account number":[
-        r"[a-zA-Z]{2}-\d[4]-[a-zA-Z]\d[5]" # is this even right ??
+        r"[a-zA-Z]{2}-\d{4}-[a-zA-Z]\d{5}" # AA-####-A#####
     ],
     "ssn":[
-        r"\d{3}-\d{2}-\d{4}"
+        r"\d{3}-\d{2}-\d{4}" # ###-##-####
     ],
     "date":[ # these are more complicated than I originally suspected, matching only valid dates instead of 00/00/0230
         r"(0[1-9]|1[0-2])\/(0[1-9]|[0-2]\d|3[0-1])\/\d{3}[1-9]", # MM/DD/YYYY
-        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d{3}[1-9]", # MM-DD-YY
-        r"(0[1-9]|1[0-2])\/(0[1-9][0-2]\d|3[0-1])\/\d[1-9]",
-        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d[1-9]",
+        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d{3}[1-9]", # MM-DD-YYYY
+        r"(0[1-9]|1[0-2])\/(0[1-9][0-2]\d|3[0-1])\/\d[1-9]", # MM/DD/YY
+        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d[1-9]", # MM-DD-YY
     ],
 }
+# Another data structure to contain the results of each search
 results = {"phone number":[0,0,0],"account number":[0],"ssn":[0],"date":[0,0,0,0]}
 with open(search_file, 'r') as sf:
     for line in sf:
@@ -48,24 +30,10 @@ with open(search_file, 'r') as sf:
                 z =re.findall(x,line) # don't need to compile, because if you call findall with a string, it just compiles the string for you... thanks python
                 for a in z: # for each match
                     results[k][ind] += 1
-                    print(f" found : {a} (on {k},{ind}) at {line}")
-#print(terms)
-print(results)
-    # b = False
-    # i = 0
-    # reg_ex = re.compile(search_term)
-    # for line in sf:
-    #     # m = re.search(reg_ex, line)
-    #     m = re.findall(reg_ex, line)
-    #     if m:
-    #         print("\nFound it!")
-    #         print(line.strip())
-    #         # print(m.group(0))
-    #         for item in m:
-    #             print(item)
-    #             i += 1
-    #         b = True
-    # if not b:
-    #     print("Didn't find it.")
-    # else:
-    #     print(f"Found {i} instances.")
+                    print(f" found : {a} (using {k}'s rule:{ind}) in the line \"{line}\"")
+print(results) # the dict shows the breakdown of which rule got matched, and the next line tallies the totals for each category
+for k in results.keys():
+    count = 0
+    for i in results[k]:
+        count += i
+    print(f"There were {count} matches for {k}")
