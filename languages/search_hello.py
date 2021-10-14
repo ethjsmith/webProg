d6c442af2cae2bc95df265f6d7cbe33cf403124d
Ethan Smith <ethan@esmithy.net>
2021-10-14 13:18:55 -0600

    made a new version that follows the actual requirements, and also updated warpeace

--- languages/search_hello.py
+++ languages/search_hello.py
@@ -3,9 +3,12 @@ search_file = 'warpeace.txt'
 # A data structure to surpass metal gear, should be easily prgrammatically iterable
 terms = {
    "phone number":[
-        r"\d{3}-\d{3}-\d{4}", # ###-###-####
-        r"1-\d{3}-\d{3}-\d{4}", # 1-###-###-####
-        r"\(\d{3}\)\s\d{3}-\d{4}", # (###) ###-####
+        #r"\d{3}-\d{3}-\d{4}", # ###-###-####
+        #r"1-\d{3}-\d{3}-\d{4}", # 1-###-###-####
+        #r"\(\d{3}\)\s\d{3}-\d{4}", # (###) ###-####
+
+        r"(1-)?\(?\d{3}\)?\s?[-\s]\d{3}-\d{4}" # this assumes that parenthese are balanced
+
     ],
     "account number":[
         r"[a-zA-Z]{2}-\d{4}-[a-zA-Z]\d{5}" # AA-####-A#####
@@ -14,17 +17,19 @@ terms = {
         r"\d{3}-\d{2}-\d{4}" # ###-##-####
     ],
     "date":[ # these are more complicated than I originally suspected, matching only valid dates instead of 00/00/0230
-        r"(0[1-9]|1[0-2])\/(0[1-9]|[0-2]\d|3[0-1])\/\d{3}[1-9]", # MM/DD/YYYY
-        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d{3}[1-9]", # MM-DD-YYYY
-        r"(0[1-9]|1[0-2])\/(0[1-9][0-2]\d|3[0-1])\/\d[1-9]", # MM/DD/YY
-        r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d[1-9]", # MM-DD-YY
+        # r"(0[1-9]|1[0-2])\/(0[1-9]|[0-2]\d|3[0-1])\/\d{3}[1-9]", # MM/DD/YYYY
+        # r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d{3}[1-9]", # MM-DD-YYYY
+        # r"(0[1-9]|1[0-2])\/(0[1-9][0-2]\d|3[0-1])\/\d[1-9]", # MM/DD/YY
+        # r"(0[1-9]|1[0-2])-(0[1-9][0-2]\d|3[0-1])-\d[1-9]", # MM-DD-YY
+
+        r"(0[1-9]|1[0-2])[\/-](0[1-9]|[0-2]\d|3[0-1])[\/-](\d{4}|[1-9]\d)"
     ],
 }
 # Another data structure to contain the results of each search
 results = {"phone number":[0,0,0],"account number":[0],"ssn":[0],"date":[0,0,0,0]}
 with open(search_file, 'r') as sf:
     for line in sf:
-        for k in terms.keys():
+        for k in terms.keys(): # step through the dictionary by key
             #print(f"searching for {k}")
             for ind,x in enumerate(terms[k]): # using enumerate to get the index as well, so that the count can be added to the results dict
                 z =re.findall(x,line) # don't need to compile, because if you call findall with a string, it just compiles the string for you... thanks python
---
+++ languages/v2.py
@@ -0,0 +1,19 @@
+import re
+search_file = 'warpeace.txt'
+
+# list contains the type of regex, the actual regex ( as a raw string so that python wont escape some of the charaters) and a count of number of times it was found
+terms = [
+    ["phone number",r"(1-)?\(?\d{3}\)?\s?[-\s]\d{3}-\d{4}",0],
+    ["account number",r"[a-zA-Z]{2}-\d{4}-[a-zA-Z]\d{5}",0],
+    ["ssn",r"\d{3}-\d{2}-\d{4}",0],
+    ["date",r"(0[1-9]|1[0-2])[\/-](0[1-9]|[0-2]\d|3[0-1])[\/-](\d{4}|[1-9]\d)",0],
+]
+with open(search_file, 'r') as sf:
+    for line in sf:
+        for term in terms: # for each type of item, match the regex in each line
+            matches = re.findall(term[1],line)
+            for match in matches:
+                term[2] += 1 # total up all the regex, ( and print where it was found )
+                print(f" Found: {term[0]} in line: {line}")
+for term in terms: # report the end result
+    print(f"Found {term[2]} {term[0]}'s")
