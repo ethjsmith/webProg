f2c8c5fba813c7f9675f5e422904613c3d309d14
Ethan Smith <ethan@esmithy.net>
2021-10-28 13:50:39 -0600

    progress

--- languages/lexer/lex.py
+++ languages/lexer/lex.py
@@ -22,13 +22,100 @@
 # Description is only included for keywords
 # You can use any programming language to code your lexer
 
+# funny meme: language with no symbols, gotta type everything as words
+
+
+import re
 def getTokens(l):
     # lexes a single line
     for char in l:
         # look for various types of things
+            print("ok")
+def isIdentifier(s): # this is like a variable ? I don't know how to make it match that ... unquoted string not matched by keyword I guess
+    if (re.match(r"[\dA-Za-z]+",s)):
+        print(f" Found Identifier :{s}")
+        return 1
+    else:
+        return 0
+def isNumber(s):
+    if (re.match(r"\d+",s)):
+        print(f" Found number :{s}")
+        return 1
+    else:
+        return 0
 
+def isOperator(s): # there are other operators that are multicharacter too I think like comparasion ==
+    if (re.match(r"[\+\-\*\/]",s)):
+        print(f" Found operator :{s}")
+        return 1
+    else:
+        return 0
+def isAssignment(s):
+    if (re.match(r"=",s)): # ez?
+        print(f" Found assignment :{s}")
+        return 1
+    else:
+        return 0
+def isComment(s): # two comments  ?
+    if (re.match(r"^[#|?].*",s)):
+        print(f" Found comment :{s}")
+        return 1
+    else:
+        return 0
+def isString(s):# this probably doesn't work
+    if (re.match(r"^\".*\"$",s)):
+        print(f" Found String :{s}")
+        return 1
+    else:
+        return 0
+def isKeyword(s): # I guess this just matches all the specific keywords in the language?
+    if (re.match(r"(Define|Update|Print)",s)):
+        print(f" Found keyword :{s}")
+        return 1
+    else:
+        return 0
+def isEnd(s): # should I make a character at the end? I don't personally like that, but it's easier ...
+
+    if (re.match(r";",s)):
+        print(f" Found endline :{s[:-1]}") # trim endline character when printing, because this is janky af 
+        return 1
+    else:
+        return 0
+def callall(s): # checks every token against a string
+    num = isNumber(s)
+    key = isKeyword(s)
+    if not num and not key:
+        id = isIdentifier(s)
+
+    op = isOperator(s)
+    asi = isAssignment(s)
+    com = isComment(s)
+    str = isString(s)
+
+    end = isEnd(s)
+
+def parseLine(l):
+    full = ""
+    str = False
+    for c in l:
+        full += c # full sequence we're checking
+        if c == "\"" or c =="?":
+            if str:
+                str = False
+                callall(full)
+                full=""
+            else:
+                str = True
+        if c ==" " and str == False:
+            #isNumber(full)
+            callall(full)
+            full = ""
+        #print(full)
+    callall(full)
+    print()
 
 
 with open("file.txt") as f: # maybe you can pass the filename to the lexer?
     for line in f.readlines():
-        print(line)
+        #print(line)
+        parseLine(line)
