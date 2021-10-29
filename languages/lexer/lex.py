eb7ab6002a7b77854c814d083f4f7ee3c595e036
Ethan Smith <ethan@esmithy.net>
2021-10-29 10:36:40 -0600

    wow, such lexer, very cool

--- languages/lexer/lex.py
+++ languages/lexer/lex.py
@@ -26,61 +26,67 @@
 
 
 import re
+
+def printtoken(t): # prints out the token in the format presented by the assignment
+    print(f"Type: {t[0]} \tValue: {t[1]}")
 def isIdentifier(s): # this is like a variable ? I don't know how to make it match that ... unquoted string not matched by keyword I guess
-    if (re.match(r"[\dA-Za-z]+",s)):
-        print(f" Found Identifier :{s}")
+    if (re.match(r"^[a-z][a-z\d]*$",s)):
+        #print(f" Found Identifier :{s}")
         return ["Identifier",s]
     else:
         return 0
 def isNumber(s):
-    if (re.match(r"\d+",s)):
-        print(f" Found number :{s}")
+    #if (re.match(r"\d+\.?\d*",s)):
+    if (re.match(r"^\d+\.?\d*$",s)): # trim the newline character LOL
+        #print(f" Found number :{s}")
         return ["Number",s]
     else:
         return 0
 
 def isOperator(s): # there are other operators that are multicharacter too I think like comparasion ==
     if (re.match(r"[\+\-\*\/]",s)):
-        print(f" Found operator :{s}")
+        #print(f" Found operator :{s}")
         return ["Operator",s]
     else:
         return 0
 def isAssignment(s):
-    if (re.match(r"=",s)): # ez?
-        print(f" Found assignment :{s}")
+    if (re.match(r"^=$",s)): # ez?
+        #print(f" Found assignment :{s}")
         return ["Assignment",s]
     else:
         return 0
 def isComment(s): # two comments  ?
-    if (re.match(r"^[#|?].*",s)):
-        print(f" Found comment :{s}")
+    if (re.match(r"^[#].*$",s)):
+        #print(f" Found comment :{s}")
         return ["Comment",s]
     else:
         return 0
 def isString(s):# this probably doesn't work
     if (re.match(r"^\".*\"$",s)):
-        print(f" Found String :{s}")
+        #print(f" Found String :{s}")
         return ["String",s]
     else:
         return 0
 def isKeyword(s): # I guess this just matches all the specific keywords in the language?
-    if (re.match(r"(Define|Update|Print)",s)):
-        print(f" Found keyword :{s}")
+    if (re.match(r"^[A-Z]+$",s)): # Keywords are UPPER case, identifiers are lower
+        #print(f" Found keyword :{s}")
         return ["Keyword",s]
     else:
         return 0
 def isEnd(s): # should I make a character at the end? I don't personally like that, but it's easier ...
 
-    if (re.match(r";",s)):
-        print(f" Found endline :{s[:-1]}") # trim endline character when printing, because this is janky af
+    if (re.match(r"^;$",s)):
+        #print(f" Found endline :{s[:-1]}") # trim endline character when printing, because this is janky af
         return ["Endline",s]
     else:
         return 0
 def callall(s): # checks every token against a string
+    if s == "":
+        return -1
     num = isNumber(s)
     key = isKeyword(s)
-    if not num and not key:
-        id = isIdentifier(s)
+    #if not num and not key:
+    id = isIdentifier(s)
 
     op = isOperator(s)
     asi = isAssignment(s)
@@ -88,16 +94,23 @@ def callall(s): # checks every token against a string
     str = isString(s)
 
     end = isEnd(s)
-    print(locals())
-    for var in locals():
-        if var and var != "s":
-            print(f"NICE::: {var[0]}")
+    # print(locals())
+    # for var in locals():
+    #     if var and var != "s":
+    #         print(f"NICE::: {var[0]}")
+    found = False
+    for token in (num,key,id,op,asi,com,str,end):
+        if token:
+            printtoken(token)
+            found = True
+    if not found: # if the token is an error
+        print(f"Type: ERROR \tValue{s}")
 def parseLine(l):
     full = ""
     str = False
     for c in l:
         full += c # full sequence we're checking
-        if c == "\"" or (c =="?" and str == False):
+        if c == "\"" or (c =="#" and str == False):
             if str:
                 str = False
                 callall(full)
@@ -106,10 +119,12 @@ def parseLine(l):
                 str = True
         if c ==" " and str == False:
             #isNumber(full)
-            callall(full)
+            a = full.rstrip()
+            callall(a)
             full = ""
         #print(full)
-    callall(full)
+    a = full.strip()
+    callall(a)
     print()
 
 
